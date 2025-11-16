07957cc698827ed65018c9a4590039980918e0e0
SaintVentura <neomashego@saintventura.co.za>
2025-11-16 22:06:44 +0200

    Add SendGrid email support as primary email service - most reliable for Render

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 const nodemailer = require('nodemailer');
 const { Resend } = require('resend');
+const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 
 const app = express();
